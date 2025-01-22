4b6ce0946b6c37d7a06a0cbf5ff4b50c9600e7e4
Valerian Revel Febrian <valerianfebrian77@gmail.com>
2025-01-22 08:43:18 +0700

    menambahkan database migration

---
+++ database/migrations/2025_01_22_010558_create_transactions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->text('Address');
+            $table->text('phone');
+            $table->date('date');
+            $table->unsignedBigInteger('court_id');
+            $table->foreign('court_id')->references('id')->on('courts');
+            $table->time('starttime');
+            $table->time('endtime');
+            $table->integer('duration');
+            $table->integer('costume');
+            $table->integer('shoes');
+            $table->integer('total');
+            $table->integer('grandtotal');
+            $table->integer('paytotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function() {
+    return ('home');
+});
+
+route::get('/profile', function()  {
+    return view('profile');
+});
+
+route::get('/profile/{$id}', function($id)  {
+    return 'hallo'.$id;
+});
+
+Route::get('/admin', function() {
+    return view('MasterProject');
+});
+
+Route::get('/mp', function() {
+    return view('mp');
+});
+
+Route::get('/ms', function() {
+    return view('ms');
+});
+
+Route::get('/homee', function() {
+    return view('homee');
+});
